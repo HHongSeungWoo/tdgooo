85fdf66296c46fd231db5c8d4b4963b500e373e9
Seungwoo Hong <qksn1541@gmail.com>
2023-03-12 21:31:03 +0900

    feat: todo app endpoint 추가

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"fiber-test/internal/config"
+	"fiber-test/internal/database"
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"log"
 )
 
 func main() {
+	config.MustInit(".env")
+	database.MustConnect()
+
 	app := fiber.New(fiber.Config{
 		JSONEncoder: json.Marshal,
 		JSONDecoder: json.Unmarshal,
-		Prefork:     true,
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(c.App().Stack())
+		return nil
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Get("/:id", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Patch("/:id", func(c *fiber.Ctx) error {
+		return nil
+	})
+
+	app.Delete("/:id", func(c *fiber.Ctx) error {
+		return nil
 	})
 
 	log.Fatal(app.Listen(":3000"))
