a966d371e9360886fdc312db83564fbe71e1e05d
Seungwoo Hong <qksn1541@gmail.com>
2023-03-13 00:17:40 +0900

    feat: 엔드포인트 구현

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 import (
 	"fiber-test/internal/config"
 	"fiber-test/internal/database"
+	"fiber-test/internal/model"
+	"fiber-test/internal/validation"
 	"github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v2"
 	"log"
+	"strconv"
 )
+
+type CreateTodo struct {
+	Title       string  `json:"title" validate:"required,min=2"`
+	Category    *string `json:"category,omitempty"`
+	Description *string `json:"description,omitempty"`
+}
+
+type UpdateTodo struct {
+	Title       *string `json:"title,omitempty" validate:"omitempty,min=2"`
+	Category    *string `json:"category,omitempty"`
+	Description *string `json:"description,omitempty"`
+}
+
+type Pagination struct {
+	Limit  int `query:"limit"`
+	Offset int `query:"offset"`
+}
 
 func main() {
 	config.MustInit(".env")
@@ -18,22 +38,119 @@ func main() {
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return nil
+		todos := &[]model.Todo{}
+		query := &Pagination{}
+		if err := c.QueryParser(query); err != nil {
+			c.Status(400)
+			return nil
+		}
+
+		if query.Limit == 0 {
+			query.Limit = 10
+		}
+
+		database.DB.Order("id desc").Limit(query.Limit).Offset(query.Offset).Find(&todos)
+		return c.JSON(todos)
 	})
 
 	app.Post("/", func(c *fiber.Ctx) error {
-		return nil
+		body := &CreateTodo{}
+
+		if err := c.BodyParser(body); err != nil {
+			return err
+		}
+
+		if err := validation.Struct(body); err != nil {
+			return err
+		}
+
+		todo := &model.Todo{
+			Title: body.Title,
+		}
+		if body.Category != nil {
+			todo.Category = model.NullString{
+				String: *body.Category,
+				Valid:  true,
+			}
+		}
+		if body.Description != nil {
+			todo.Description = model.NullString{
+				String: *body.Description,
+				Valid:  true,
+			}
+		}
+		database.DB.Save(todo)
+
+		return c.JSON(&todo)
 	})
 
 	app.Get("/:id", func(c *fiber.Ctx) error {
-		return nil
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			c.Status(404)
+			return nil
+		}
+		todo := &model.Todo{Id: uint(id)}
+		if database.DB.Find(todo).RowsAffected != 1 {
+			c.Status(404)
+			return nil
+		}
+
+		return c.JSON(todo)
 	})
 
 	app.Patch("/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			c.Status(404)
+			return nil
+		}
+
+		body := &UpdateTodo{}
+
+		if err = c.BodyParser(body); err != nil {
+			return err
+		}
+
+		if err = validation.Struct(body); err != nil {
+			return err
+		}
+
+		todo := &model.Todo{Id: uint(id)}
+		if database.DB.Find(todo).RowsAffected != 1 {
+			c.Status(404)
+			return nil
+		}
+
+		if body.Title != nil {
+			todo.Title = *body.Title
+		}
+		if body.Category != nil {
+			todo.Category = model.NullString{
+				String: *body.Category,
+				Valid:  true,
+			}
+		}
+		if body.Description != nil {
+			todo.Description = model.NullString{
+				String: *body.Description,
+				Valid:  true,
+			}
+		}
+
+		database.DB.Save(todo)
 		return nil
 	})
 
 	app.Delete("/:id", func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			c.Status(404)
+			return nil
+		}
+		todo := &model.Todo{Id: uint(id)}
+		database.DB.Delete(todo)
+
 		return nil
 	})
 
