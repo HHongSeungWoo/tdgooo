764cb96c0978b56aec3426d3857268400844acdc
Seungwoo Hong <qksn1541@gmail.com>
2023-03-12 23:36:05 +0900

    feat: todo model null 구현

--- internal/model/todo.go
+++ internal/model/todo.go
@@ -2,14 +2,40 @@ package model
 
 import (
 	"database/sql"
+	"database/sql/driver"
+	"github.com/goccy/go-json"
 	"time"
 )
 
+type NullString sql.NullString
+
+func (x *NullString) MarshalJSON() ([]byte, error) {
+	if !x.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(x.String)
+}
+
+func (x *NullString) Scan(value interface{}) error {
+	var s sql.NullString
+	if err := s.Scan(value); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (x NullString) Value() (driver.Value, error) {
+	if !x.Valid {
+		return nil, nil
+	}
+	return x.String, nil
+}
+
 type TODO struct {
-	Id          uint
-	Title       string
-	Category    sql.NullString
-	Description sql.NullString
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Id          uint       `gorm:"primaryKey" json:"id"`
+	Title       string     `json:"title"`
+	Category    NullString `json:"category"`
+	Description NullString `json:"description"`
+	CreatedAt   time.Time  `gorm:"<-:create" json:"createdAt"`
+	UpdatedAt   time.Time  `gorm:"autoUpdateTime" json:"updatedAt"`
 }
